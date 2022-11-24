9a4a172bfd6587b34a7349c01a7c06bb9f93da25
DESKTOP-JBOAJ6M\George <mathmilos3@gmail.com>
2022-11-24 22:17:35 +0200

    Improved SurroundWall

--- frontend/src/app/pages/wall/wall.component.ts
+++ frontend/src/app/pages/wall/wall.component.ts
@@ -7,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WallComponent implements OnInit {
 
-  constructor() { }
+  song_title:  string = "Blinding Lights"; //Might change this with database later
+  song_artist: string = "The Weeknd";
+
+  lyrics: string[] = [];
+
+  constructor() { 
+    this.lyrics.push("I've been on my own for long enough");
+    this.lyrics.push("Maybe you can show me how to love, maybe");
+    this.lyrics.push("I'm going through withdrawals");
+  }
 
   ngOnInit(): void {
   }
