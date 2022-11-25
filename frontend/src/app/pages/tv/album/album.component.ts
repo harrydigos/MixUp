4e2f7a8cac03babe5024a26a3be845f086f63708
BabisDigos <ntigos2001@gmail.com>
2022-11-25 19:24:22 +0200

    Album page created

--- frontend/src/app/pages/tv/album/album.component.ts
+++ frontend/src/app/pages/tv/album/album.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { TvService } from 'src/app/services/tv.service';
 
@@ -7,9 +8,13 @@ import { TvService } from 'src/app/services/tv.service';
   styleUrls: ['./album.component.scss'],
 })
 export class AlbumComponent implements OnInit {
-  constructor(private tvService: TvService) {
+  constructor(private tvService: TvService, private location: Location) {
     this.tvService.setNavState('hide');
   }
 
   ngOnInit(): void {}
+
+  back() {
+    this.location.back();
+  }
 }
