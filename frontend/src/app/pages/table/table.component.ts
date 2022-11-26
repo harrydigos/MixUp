8d7ffb6a9a9e5259d89491b8c33ebbd5bfe365cf
BabisDigos <ntigos2001@gmail.com>
2022-11-26 18:33:55 +0200

    Created table layout and a toggling box as menu for now

--- frontend/src/app/pages/table/table.component.ts
+++ frontend/src/app/pages/table/table.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  styleUrls: ['./table.component.scss'],
 })
 export class TableComponent implements OnInit {
+  menuState: boolean = false;
+  mouseX: number | undefined;
+  mouseY: number | undefined;
+  @ViewChild('table') table: ElementRef | undefined;
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  toggleMenu(): void {
+    console.log('toggleMenu()');
+    if (this.menuState) {
+      this.renderer.removeChild(
+        this.table?.nativeElement,
+        this.table?.nativeElement.lastChild
+      );
+      this.menuState = false;
+      return;
+    }
+
+    console.log(this.mouseX, this.mouseY);
+    this.menuState = true;
+    const menu: HTMLElement = this.renderer.createElement('div');
+    menu.id = 'menu';
+    menu.className = 'menu';
+    menu.style.top = `${this.mouseY}px`;
+    menu.style.left = `${this.mouseX}px`;
+    menu.style.transform = `translate(-50%, -50%)`;
+
+    this.table?.nativeElement.appendChild(menu);
   }
 
+  getMousePosition = (event: MouseEvent) => {
+    this.mouseX = event.clientX;
+    this.mouseY = event.clientY;
+  };
 }
