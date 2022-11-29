1707b557e1ba6f1b9d1d084c3d4ad2280c2badb8
BabisDigos <ntigos2001@gmail.com>
2022-11-29 13:15:08 +0200

    Cool angular stuff :')

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,20 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TasksComponent } from './pages/tasks/tasks.component';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { environment } from 'src/environments/environment';
+
+import { TasksComponent } from './pages/tasks/tasks.component';
 import { ItemShopComponent } from './pages/item-shop/item-shop.component';
 import { ItemPreviewComponent } from './pages/item-shop/item-preview/item-preview.component';
+
 import { TableComponent } from './pages/table/table.component';
 import { WallComponent } from './pages/wall/wall.component';
 import { PhoneComponent } from './pages/phone/phone.component';
 import { TVComponent } from './pages/tv/tv.component';
-import { TvNavbarComponent } from './components/tv/tv-navbar/tv-navbar.component';
 
-import { HomeComponent } from './pages/phone/home/home.component';
-import { PhoneCardComponent } from './components/phone/phone-card/phone-card.component';
-import { NewTracksComponent } from './components/phone/new-tracks/new-tracks.component';
+import { TvNavbarComponent } from './components/tv/tv-navbar/tv-navbar.component';
 import { TableDiscComponent } from './components/table/table-disc/table-disc.component';
 
 const socketIoConfig: SocketIoConfig = { url: environment.host, options: {} };
@@ -27,14 +26,12 @@ const socketIoConfig: SocketIoConfig = { url: environment.host, options: {} };
     ItemShopComponent,
     ItemPreviewComponent,
 
-    TvNavbarComponent,
     TVComponent,
     TableComponent,
     WallComponent,
     PhoneComponent,
-    HomeComponent,
-    PhoneCardComponent,
-    NewTracksComponent,
+
+    TvNavbarComponent,
     TableDiscComponent,
   ],
   providers: [],
--- frontend/src/app/pages/phone/phone.module.ts
+++ frontend/src/app/pages/phone/phone.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhoneRoutingModule } from './phone-routing.module';
+
 import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+
 import { PhoneCardComponent } from 'src/app/components/phone/phone-card/phone-card.component';
 import { NewTracksComponent } from 'src/app/components/phone/new-tracks/new-tracks.component';
-import { SearchComponent } from './search/search.component';
 import { PhNavbarComponent } from 'src/app/components/phone/ph-navbar/ph-navbar.component';
 import { SongListComponent } from 'src/app/components/phone/song-list/song-list.component';
-
 
 @NgModule({
   declarations: [
     HomeComponent,
+    SearchComponent,
+
+    SongListComponent,
     PhoneCardComponent,
     NewTracksComponent,
-    SearchComponent,
     PhNavbarComponent,
-    SongListComponent
   ],
   imports: [CommonModule, PhoneRoutingModule],
 })
