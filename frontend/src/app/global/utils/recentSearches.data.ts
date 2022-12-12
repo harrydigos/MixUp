4e5763cc551fab9ec0a929e2b79a05166cb42130
DESKTOP-JBOAJ6M\George <mathmilos3@gmail.com>
2022-12-12 18:10:14 +0200

    minor changes

--- frontend/src/app/global/utils/recentSearches.data.ts
+++ frontend/src/app/global/utils/recentSearches.data.ts
@@ -18,7 +18,7 @@ export const recentSearches: AlbumDummyModel[] = [
   {
     name: 'Blinding Lights',
     artist: 'The Weeknd',
-    image: 'blindingPhone.jpg',
+    image: 'blinding_lights.jpg',
     isFavorite: false,
     noTracks: 0,
   },
