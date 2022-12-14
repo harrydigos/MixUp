03b3c0875144c802be8aeda1c9df40fb1fbc99e8
BabisDigos <ntigos2001@gmail.com>
2022-12-14 17:07:13 +0200

    Rotational disc

--- frontend/src/app/pages/table/table.component.ts
+++ frontend/src/app/pages/table/table.component.ts
@@ -1,13 +1,9 @@
-import {
-  Component,
-  ElementRef,
-  OnInit,
-  Renderer2,
-  ViewChild,
-} from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { AlbumModel } from 'src/app/global/models';
 import { AlbumsService } from 'src/app/global/services';
 
+let id: any = null;
+let state: number = 0;
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
@@ -26,16 +22,30 @@ export class TableComponent implements OnInit {
   @ViewChild('favorites') favorites?: ElementRef;
   @ViewChild('queue') queue?: ElementRef;
 
+  @ViewChild('disc') disc?: ElementRef;
+
   mouseX: number | undefined;
   mouseY: number | undefined;
 
   isPlaying: boolean = true;
   volume: boolean = true;
 
-  constructor(
-    private renderer: Renderer2,
-    private albumsService: AlbumsService
-  ) {}
+  // Disc Rotation
+
+  // var init, rotate, start, stop,
+  active: boolean = false;
+  angle: number = 0;
+  rotation: number = 0;
+
+  startAngle: number = 0;
+  center: { x: number; y: number } = {
+    x: 0,
+    y: 0,
+  };
+  R2D: number = 180 / Math.PI;
+  styleRotation: string = '';
+
+  constructor(private renderer: Renderer2, private albumsService: AlbumsService) {}
 
   ngOnInit(): void {
     this.albumsService.getAll().subscribe((result) => {
@@ -56,11 +66,7 @@ export class TableComponent implements OnInit {
     if (this.mouseX! > 1750 || this.mouseY! > 980) return;
 
     this.renderer.setStyle(this.menu!.nativeElement, 'top', `${this.mouseY}px`);
-    this.renderer.setStyle(
-      this.menu!.nativeElement,
-      'left',
-      `${this.mouseX}px`
-    );
+    this.renderer.setStyle(this.menu!.nativeElement, 'left', `${this.mouseX}px`);
     this.renderer.addClass(this.menu!.nativeElement, '__show');
   }
 
@@ -95,9 +101,63 @@ export class TableComponent implements OnInit {
 
   togglePlay(): void {
     this.isPlaying = !this.isPlaying;
+    if (this.isPlaying) {
+      clearInterval(id);
+      state = (this.angle + this.rotation) % 360;
+    }
   }
 
   toggleMute(): void {
     this.volume = !this.volume;
+  }
+
+  start(e: MouseEvent) {
+    if (this.active) return;
+    e.preventDefault();
+    let b = this.disc!.nativeElement.getBoundingClientRect();
+
+    this.center = {
+      x: b.left + b.width / 2,
+      y: b.top + b.height / 2,
+    };
+
+    let x = e.clientX - this.center.x;
+    let y = e.clientY - this.center.y;
+    this.startAngle = this.R2D * Math.atan2(y, x);
+    this.active = true;
+  }
+
+  rotate(e: MouseEvent): void {
+    if (!this.active) return;
+    e.preventDefault();
+    let x = e.clientX - this.center.x;
+    let y = e.clientY - this.center.y;
+    let d = this.R2D * Math.atan2(y, x);
+    this.rotation = d - this.startAngle;
+    this.styleRotation = `rotate(${this.angle + this.rotation}deg)`;
+    this.disc!.nativeElement.style.cursor = 'grabbing';
+  }
+
+  stop(): void {
+    this.angle += this.rotation;
+    this.active = false;
+    this.disc!.nativeElement.style.cursor = 'grab';
+  }
+
+  playAnim(): void {
+    if (!this.isPlaying || this.active) return;
+    console.log(state);
+    let rotate = state ? state : this.angle + this.rotation;
+    const frame = () => {
+      if (rotate === 360) {
+        clearInterval(id);
+      } else {
+        rotate++;
+        state = rotate;
+        this.styleRotation = `rotate(${state}deg)`;
+      }
+    };
+
+    id = setInterval(frame, 10);
   }
 }
