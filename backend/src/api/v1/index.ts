b922db2d5c86b050af685cce14fc888d92457401
BabisDigos <ntigos2001@gmail.com>
2023-01-07 19:07:40 +0200

    Now ok

--- backend/src/api/v1/index.ts
+++ backend/src/api/v1/index.ts
@@ -1,19 +1,10 @@
 import * as express from "express";
 import { AlbumController } from "./albums/albums.controller";
-import { ExampleController } from "./example/example.controller";
-import { ItemShopController } from "./item-shop/item-shop.controller";
-import { TaskController } from "./task/task.controller";
 import { SongsController } from "./songs/songs.controller";
 const apiV1Router = express.Router();
 
 apiV1Router
-  // Example routes
-  .use("/example", new ExampleController().applyRoutes())
-  .use("/item-shop", new ItemShopController().applyRoutes())
-  .use("/tasks", new TaskController().applyRoutes())
-
-  // Our routes
-
+  // Routes
   .use("/albums", new AlbumController().applyRoutes())
   .use("/songs", new SongsController().applyRoutes());
 
