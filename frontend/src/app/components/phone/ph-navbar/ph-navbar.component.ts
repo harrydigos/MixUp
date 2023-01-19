d93caaa64d3cb40c5b641965c21dc9550f6291bc
BabisDigos <ntigos2001@gmail.com>
2023-01-19 16:47:39 +0200

    Improved code for phone & tv navbars

--- frontend/src/app/components/phone/ph-navbar/ph-navbar.component.ts
+++ frontend/src/app/components/phone/ph-navbar/ph-navbar.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NavbarState } from 'src/app/global/models';
 
 @Component({
   selector: 'app-ph-navbar',
-  templateUrl: './ph-navbar.component.html',
-  styleUrls: ['./ph-navbar.component.scss'],
+  template: `
+    <nav *ngIf="page !== 'hide'" class="relative z-50">
+      <div class="fixed bottom-0 flex h-16 w-full items-center border-t-2 border-blue bg-blue-dark">
+        <div class="flex w-full justify-around text-xs font-normal">
+          <button routerLink="/phone/home" class="flex flex-col items-center">
+            <img
+              class="py-1"
+              src="assets/Icons/Home{{ page === 'home' ? 'Bulk' : 'Twotone' }}.svg"
+              width="24px"
+              height="24px"
+            />
+            <p [ngClass]="page === 'home' ? 'text-[#A5C9CA]' : 'text-white'">Home</p>
+          </button>
+
+          <button routerLink="/phone/search" class="flex flex-col items-center">
+            <img
+              class="py-1"
+              src="assets/Icons/Search{{ page === 'search' ? 'Bulk' : 'Twotone' }}.svg"
+              width="24px"
+              height="24px"
+            />
+            <p [ngClass]="page === 'search' ? 'text-[#A5C9CA]' : 'text-white'">Search</p>
+          </button>
+
+          <button routerLink="/phone/favorites" class="flex flex-col items-center">
+            <img
+              class="py-1"
+              src="assets/Icons/Library{{ page === 'library' ? 'Bulk' : 'Twotone' }}.svg"
+              width="24px"
+              height="24px"
+            />
+            <p [ngClass]="page === 'library' ? 'text-[#A5C9CA]' : 'text-white'">Library</p>
+          </button>
+        </div>
+      </div>
+    </nav>
+  `,
 })
-export class PhNavbarComponent implements OnInit {
+export class PhNavbarComponent {
   @Input() page: NavbarState = 'home';
 
   constructor() {}
-
-  ngOnInit(): void {}
 }
--- frontend/src/app/components/tv/tv-navbar/tv-navbar.component.ts
+++ frontend/src/app/components/tv/tv-navbar/tv-navbar.component.ts
@@ -1,98 +1,55 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NavbarState } from 'src/app/global/models';
 
 @Component({
   selector: 'app-tv-navbar',
   template: `
     <nav *ngIf="page !== 'hide'" class="relative top-0 left-0 z-50 h-full w-full select-none">
-      <div class="fixed top-0 p-[100px] h-full flex flex-col justify-between">
+      <div class="fixed top-0 flex h-full flex-col justify-between p-[100px]">
         <button routerLink="/tv/home">
           <img src="assets/logo.svg" />
         </button>
         <div class="flex flex-col gap-6">
-          <button
-            *ngIf="page === 'home'; else elseHome"
-            routerLink="/tv/home"
-            class="flex h-20 w-20 items-center justify-center rounded-xl p-4 group bg-blue"
-          >
+          <button routerLink="/tv/home" class="group flex h-20 w-20 items-center justify-center rounded-xl bg-blue p-4">
             <img
               class="duration-100 group-hover:rotate-12"
-              src="assets/Icons/HomeBulk.svg"
+              src="assets/Icons/Home{{ page === 'home' ? 'Bulk' : 'Twotone' }}.svg"
               width="48px"
               height="48px"
             />
           </button>
-          <ng-template #elseHome>
-            <button routerLink="/tv" class="flex h-20 w-20 items-center justify-center rounded-xl p-4 group bg-blue">
-              <img
-                class="duration-100 group-hover:rotate-12"
-                src="assets/Icons/HomeTwotone.svg"
-                width="48px"
-                height="48px"
-              />
-            </button>
-          </ng-template>
 
           <button
-            *ngIf="page === 'search'; else elseSearch"
             routerLink="/tv/search"
-            class="flex h-20 w-20 items-center justify-center rounded-xl p-4 group bg-blue"
+            class="group flex h-20 w-20 items-center justify-center rounded-xl bg-blue p-4"
           >
             <img
               class="duration-100 group-hover:rotate-12"
-              src="assets/Icons/SearchBulk.svg"
+              src="assets/Icons/Search{{ page === 'search' ? 'Bulk' : 'Twotone' }}.svg"
               width="48px"
               height="48px"
             />
           </button>
-          <ng-template #elseSearch>
-            <button
-              routerLink="/tv/search"
-              class="flex h-20 w-20 items-center justify-center rounded-xl p-4 group bg-blue"
-            >
-              <img
-                class="duration-100 group-hover:rotate-12"
-                src="assets/Icons/SearchTwotone.svg"
-                width="48px"
-                height="48px"
-              />
-            </button>
-          </ng-template>
+
           <button
-            *ngIf="page === 'library'; else elseLibrary"
             routerLink="/tv/library"
-            class="flex h-20 w-20 items-center justify-center rounded-xl p-4 group bg-blue"
+            class="group flex h-20 w-20 items-center justify-center rounded-xl bg-blue p-4"
           >
             <img
               class="duration-100 group-hover:rotate-12"
-              src="assets/Icons/LibraryBulk.svg"
+              src="assets/Icons/Library{{ page === 'library' ? 'Bulk' : 'Twotone' }}.svg"
               width="48px"
               height="48px"
             />
           </button>
-          <ng-template #elseLibrary>
-            <button
-              routerLink="/tv/library"
-              class="flex h-20 w-20 items-center justify-center rounded-xl p-4 group bg-blue"
-            >
-              <img
-                class="duration-100 group-hover:rotate-12"
-                src="assets/Icons/LibraryTwotone.svg"
-                width="48px"
-                height="48px"
-              />
-            </button>
-          </ng-template>
         </div>
         <img src="assets/images/profile.jpg" class="h-20 w-20 cursor-pointer rounded-full object-cover object-center" />
       </div>
     </nav>
   `,
 })
-export class TvNavbarComponent implements OnInit {
+export class TvNavbarComponent {
   @Input() page: NavbarState = 'home';
 
   constructor() {}
-
-  ngOnInit(): void {}
 }
